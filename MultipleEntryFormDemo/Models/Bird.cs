767882835940bd1c8cf92cf7b60b606274719304
Brian Bird <birdb@lanecc.edu>
2023-04-25 16:58:06 -0700

    Added jQuery validation

--- MultipleEntryFormDemo/Models/Bird.cs
+++ MultipleEntryFormDemo/Models/Bird.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace MultipleEntryFormDemo.Models
 {
 	public class Bird
 	{
 		public int BirdId { get; set; }
-		public String Name { get; set; }
-		public String Order { get; set; }
+		[Required]
+		public String Name { get; set; } = string.Empty;
+		[Required]
+		public String Order { get; set; } = string.Empty;
+		[Required]
 		public int Number { get; set; }
 	}
 }
--- MultipleEntryFormDemo/Models/Sighting.cs
+++ MultipleEntryFormDemo/Models/Sighting.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace MultipleEntryFormDemo.Models
 {
 	public class Sighting
 	{
 		private List<Bird> birds = new List<Bird>();
 		public int SightingId { get; set; }
-		public String Location { get; set; }
-		public String Birder { get; set; }
+		[Required]
+		public String Location { get; set; } = string.Empty;
+		[Required]
+		public String Birder { get; set; } = string.Empty;
 		public DateOnly Date { get; set; }
 		public List<Bird> Birds { get { return birds; } }
 	}
